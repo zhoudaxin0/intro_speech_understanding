ae1bfd43fec87cb1d30d79ab1da0cc0b4e6bb561
zhoudaxin0 <148180713+zhoudaxin0@users.noreply.github.com>
2024-01-25 14:42:18 +0900

    Update homework12.py

--- 2023_fall/lec12/homework12.py
+++ 2023_fall/lec12/homework12.py
@@ -9,5 +9,6 @@ def synthesize(text, lang, filename):
     lang (str) - the language in which you want to synthesize it
     filename (str) - the filename in which it should be saved
     '''
-    raise RuntimeError("FAIL! You need to change this function so that it works!")
+    tts = gtts.gTTS(text=text, lang=lang)
+    tts.save(filename)
 
